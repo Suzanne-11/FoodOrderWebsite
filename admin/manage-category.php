83ffb9b35746420d5c8e4b09975cc11cb705b382
Suzanne-11 <suzannewdmello@gmail.com>
2022-01-27 08:18:04 +0530

    uploading image functionality complete

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -19,37 +19,81 @@
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
                     </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>Suzanne Dmello</td>
-                        <td>SuzDmello</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>Suzanne Dmello</td>
-                        <td>SuzDmello</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>Suzanne Dmello</td>
-                        <td>SuzDmello</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+
+                    <?php 
+                        //query to get all categories from db
+                        $sql = "SELECT * FROM category";
+
+                        //Execute query
+                        $res = mysqli_query($conn, $sql);
+
+                        $count = mysqli_num_rows($res);
+                        //create serial number variable
+                        $sn = 1;
+
+                        if($count>0){
+                            //get the data n display
+                            while($row = mysqli_fetch_assoc($res)){
+                                $id = $row['id'];
+                                $title= $row['title'];
+                                $img_name = $row['img_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+
+                                ?>
+                                
+                                <tr>
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php echo $title; ?></td>
+
+                                    <td>
+                                        <?php 
+                                            //check if img name is available ot not
+                                            if($img_name != ""){
+                                                //display img
+                                                ?>
+                                                <img src="<?php echo SITEURL; ?>images/category/<?php echo  $img_name; ?>" width = "100px">  
+                                                
+                                                <?php
+                                            }
+                                            else{
+                                                //display msg
+                                                echo "<div class= 'error' >Image not Added</div>";
+                                            }
+                                        ?>
+                                    </td>
+
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="#" class="btn-secondary">Update Category</a>
+                                        <a href="#" class="btn-danger">Delete Category</a>
+                                    </td>
+                                </tr>
+                                
+                                <?php
+                            }
+                        }
+                        else{
+                            //will display msg inside table
+                            ?>
+                                <tr>
+                                    <td colspan = "6">
+                                        <div  class="error">No Category added.</div>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+
+                    ?>
+                    
+                    
                 </table>
 
 
